a32574262031cb8384c20becc2f3eb2c3f52df0a
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2024-06-18 18:24:27 +0500

    some bug  and error fixed

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "http://localhost:5000/api";
 
 export const getItems = async () => {
   const response = await axios.get(`${API_URL}/items`);
@@ -16,3 +16,5 @@ export const deleteItem = async (id) => {
   const response = await axios.delete(`${API_URL}/items/${id}`);
   return response.data;
 };
+
+
